3ecd2ad218170ef163968d1f00e1d0a2ccd6360e
thankinson <t_hankinson@outlook.com>
2022-03-19 16:55:28 +0000

    working api call and log to screen

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../globalStyles/global.css"
 const { REACT_APP_API_KEY } = process.env
 
 export const SearchApi = () =>{
@@ -10,13 +11,12 @@ export const SearchApi = () =>{
             const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${REACT_APP_API_KEY}&query=${search}`);
             const data = await response.json();
             console.log(data)
-            setMovie(data)
+            setMovie(data.results)
             } catch(errorLog){
                 console.log(errorLog)
             }
         }
-        
-
+ 
         const submitHandler = (e) => {
             e.preventDefault();
             MovieApi()
@@ -25,12 +25,12 @@ export const SearchApi = () =>{
     return(
         <>
         <div>
-            {!movie &&<form onSubmit={submitHandler }>
+            <form onSubmit={submitHandler }>
                 <input placeholder="Search Movie Api" type="search" onChange={(e)=> setSearch(e.target.value)} />
                 <button>Search DB</button>
-            </form>}
+            </form>
             <div>
-            {movie && <h1>{movie.original_title}</h1>}
+            {movie && movie.map((item, index) => <p>{movie[index].original_title}</p>)}
             </div>
         </div>
         </>
