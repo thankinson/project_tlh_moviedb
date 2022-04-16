87bb249f8e70acf692292804ef982c4e935a2b2c
thankinson <t_hankinson@outlook.com>
2022-04-16 16:17:50 +0100

    added new componants to package.json. Built new colapsible film list in search

--- src/components/movieresult.jsx
+++ src/components/movieresult.jsx
@@ -1,5 +1,6 @@
 import { React, useState} from "react";
-import Collapsible from "react-collapsible";
+// import Collapsible from "react-collapsible";
+import Collapse from "react-css-collapse";
 // utils
 import { addMovie } from "../utils";
 
@@ -8,9 +9,7 @@ import styled from "styled-components";
 import "../globalStyles/global.css"
 
 export const Movieresults = ({movie, idArray}) =>{
-    // console.log("checkmovie......... " + checkMovie)
-    // console.log("movie......... " + movie)
-    // console.log("idArray on movie results ......... " + idArray)
+    const [openItemIndex, setOpenItemIndex] = useState(undefined);
 
     const [film, setFilm] = useState({
         id: '',
@@ -22,91 +21,125 @@ export const Movieresults = ({movie, idArray}) =>{
             console.log(film);
             addMovie(film)
         }
+
+        function toggle(id) {
+            setOpenItemIndex(openItemIndex === id ? undefined : id);
+          }
   
     return(
         <>
+            <PageContainerDiv>
+            <MovieList>
+                {movie && movie.map((movie, index) => 
+                    <li key={index}>
+                        <MovieDropdownButton type="button" onClick={()=> toggle(movie)}>{movie.original_title}</MovieDropdownButton>
+                        <C isOpen={openItemIndex === movie}>
+                            <ContentDiv>
+                                <PosterDiv>
+                                    <ImgPoster src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.original_title} Poster`} />
+                                </PosterDiv>
+                                <MovieInfoDiv>
+                                    <p>{movie.original_title}</p>
+                                    <p>{movie.release_date}</p>
+                                    <p>{movie.overview}</p>
+                                        <ButtonDiv>
+                                        <form onSubmit={submitHandler}>
+                                                {idArray.includes(JSON.stringify(movie.id)) 
+                                                    ? <InDbPara><p>In Database</p></InDbPara>
+                                                    : <ButtonAdd onClick={()=> 
+                                                            setFilm({id: movie.id,
+                                                                    title: movie.original_title, 
+                                                                    poster: movie.poster_path})
+                                                                    }>
+                                                                Add to Collection
+                                                        </ButtonAdd>
+                                        }
+                                            </form>
+                                        </ButtonDiv>
+                                </MovieInfoDiv>
+                            </ContentDiv>
+                        </C>
+                    </li>
+                )}
 
-        <DivResults>
-        {movie && movie.map((movie, index) => 
-            
-            <DivCollapse key={index}>
-                <Collapsible  trigger={movie.original_title}>
-                    <DivContent>
-                        <DivPoster>
-                            <ImgPoster src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.original_title} Poster`} />
-                        </DivPoster>
-                        <form onSubmit={submitHandler}>
-                        <DivInformation>
-                            <p>Movie ID: {movie.id}</p>
-                            <p>Released: {movie.release_date}</p>
-                            <p>Synopsis:</p>
-                            <p>{movie.overview}</p>
-                            <p>Raiting: {movie.vote_average}</p>
-                            <ButtonDiv>
-                            {/* {console.log(JSON.stringify(movie.id)) && <p>test</p>} */}
-                            {/* this can be changed to an integer in schema  */}
-                            {idArray.includes(JSON.stringify(movie.id)) ? <InDbPara><p>In Database</p></InDbPara>
-                                                : <ButtonAdd onClick={()=> setFilm({id: movie.id,title: movie.original_title, poster: movie.poster_path})}>Add to Collection</ButtonAdd>
-                                                }
-                            </ButtonDiv>
-                        </DivInformation>
-                        </form>
-                    </DivContent>                 
-                </Collapsible >
-            </DivCollapse>)}
-        </DivResults>
+            </MovieList>
+            </PageContainerDiv>
         
         </>
 
     )
 };
 
-const DivResults = styled.div`
+const PageContainerDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+`;
+const MovieList = styled.ul`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    width: 100vw;
-    background-color: #131516;
-`
-const DivCollapse = styled.div`
-    display: flex;
-    flex-direction: row;
     width: 50vw;
-    margin-top: 1vh;
-    margin-left: 1vw;
-    margin-bottom: 1vh;
-    background-color: #222831;
-    box-shadow: 0px 0px 2px 1px white;
-`
-const DivContent = styled.div`
+    list-style: none;
+    padding: 0;
+   
+`;
+const MovieDropdownButton = styled.button`
+      transition: all 0.5s ease-in-out;
+        width: 100%;
+        padding: 20px;
+        font-size: 1em;
+        color: #c8c8c8;
+        text-align: left;
+        border: 0px solid #2e444e;
+        border-bottom: 1px solid #223239;       
+        cursor: pointer;
+        background-color: #222831;
+
+  @media screen and (max-width: 1000px) {
+    font-size: 0.9em;
+    max-height: 5em;
+  }
+
+  &:hover {
+    color: white;
+    background-color: #5a7077;
+    transition: 200ms;
+  }
+`;
+const C = styled(Collapse)`
+  transition: height 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`;
+
+const ContentDiv = styled.div`
     display: flex;
     flex-direction: row;
-    width: 100%;
-    margin-left: 1vw;
-    margin-bottom: 1vh;
-`
-const DivPoster = styled.div`
-    height: 450px;
-    width: 300px;
-`
+    max-width: 100%;
+    padding: 20px;
+    font-size: 0.9em;
+    color: #2e444e;
+    background-color: #2c3543;
+    
+`;
+const PosterDiv = styled.div`
+`;
+const MovieInfoDiv = styled.div`
+margin-left: 20px;
+font-size: 1.5em;
+`;
 const ImgPoster = styled.img`
     max-width: 300px;
-`
-const DivInformation = styled.div`
-    width: 30vw;
-    padding-left: 1vw;
-    font-size: 20px;
-
-`
+`;
 const ButtonDiv = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
-`
+`;
 const ButtonAdd = styled.button`
     width: 200px;
     height: 50px;
-`
+`;
 const InDbPara = styled.div`
     display: flex;
     flex-direction: row;
@@ -117,32 +150,4 @@ const InDbPara = styled.div`
     height: 50px;
     border-radius: 5px;
     background-color: #323634;
-`
-        // const CheckDB = ({check}) => {
-        //     for (let i = 0; i < checkMovie.length; i++) {
-        //             if (checkMovie[i].tmdbId === check){
-        //                 return (
-        //                     <><p>{check} is in database</p></>
-        //                 )
-        //             } 
-        //         }
-        // }
-        //                       {/* {movie ? setCheck(movie.id)} */}
-        //                            {/* {!movie && <CheckDB  check={movie.id}/>} */}
-        // {checkMovie.find(x => (x.tmdbId !== movie.id)) ? <p>Movie in database</p> : <ButtonAdd onClick={()=> setFilm({id: movie[index].id,title: movie[index].original_title, poster: movie[index].poster_path})}>Add to Collection</ButtonAdd> }
-
-        // {/*                                 
-        //                         {movie.every(i => idArray.includes(obj => obj === i.id)) ? <p>movie in database</p>
-        //                                                 : <ButtonAdd onClick={()=> setFilm({id: movie[index].id,title: movie[index].original_title, poster: movie[index].poster_path})}>Add to Collection</ButtonAdd>} */}
-
-                // const CheckArray = () =>{
-        //     const temp = [""]
-            
-        //     for ( let i = 0; i < checkMovie.length; i++ ){
-        //             temp.push(checkMovie[i].tmdbId)
-        //     }
-        //     setCheck(temp)
-
-        // }
-        // CheckArray()
-                               
+`;
